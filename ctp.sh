c6d35388d4dc8cbb8ceee29973a9ebd3d06098fc
Mert Şişmanoğlu <mert190737fb@gmail.com>
2021-07-07 17:29:00 +0300

    Update for an typing issue.

--- ctp.sh
+++ ctp.sh
@@ -3,7 +3,7 @@
 if [ $1 ]; then
     mightBeFile=$1
     if [ -f $mightBeFile ]; then
-        sudo cp mightBeFile /usr/local/bin/
+        sudo cp $mightBeFile /usr/local/bin/
     else
         echo "$mightBeFile is not a file."
     fi
