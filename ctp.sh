d23fc952bf62c0da1bcf943ff6a28d95b746252e
Mert Şişmanoğlu <mert190737fb@gmail.com>
2022-03-08 21:58:30 +0300

    Ctp rewrite with long term support.

--- ctp.sh
+++ ctp.sh
@@ -1,10 +1,56 @@
 #!/bin/bash
+init(){
+    inputText="Do you want to copy the file without extension?\n[yes - y] to accept, [no - n] to refuse and other keys to exit."
+}
 
-if [ $1 ]; then
-    mightBeFile=$1
-    if [ -f $mightBeFile ]; then
-        sudo cp $mightBeFile /usr/local/bin/
+splitFile() {
+    local file=$1
+    if [ -f $file ]; then
+        directory="$(dirname "$file")/"
+        filename=$(basename -- "$file")
+        extension="${filename##*.}"
+        filename="${filename%.*}"
     else
-        echo "$mightBeFile is not a file."
+        echo "$1 is not a file"
+        exit 1
     fi
-fi
+    
+}
+
+getInput(){
+    echo -e $inputText
+    read answer
+}
+
+checkInput(){
+    if [[ $answer = "yes" || $answer = "y" ]]; then
+        return 0
+        elif [[ $answer = "no" || $answer = "n" ]]; then
+        return 1
+    else
+        echo "Exitted"
+        exit 130
+    fi
+}
+
+copy(){
+    if [ checkInput ];then
+        sudo cp "${pwd}${directory}${filename}.${extension}" "/usr/local/bin/${filename}"
+    else
+        sudo cp "${pwd}${directory}${filename}.${extension}" "/usr/local/bin/${filename}.${extension}"
+    fi
+}
+function main() {
+    init
+    if [ ! $1 ]; then
+        echo "No arguments provided!"
+        exit 1
+    else
+        splitFile $1
+        getInput $inputText
+        checkInput
+        copy $1
+    fi
+}
+
+main $1
